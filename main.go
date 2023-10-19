d7e18d5ac7e820dd18d1b9fa18eef2458950f936
Zin Yan <zwy@mit.com.mm>
2023-10-19 14:39:43 +0630

    2nd commit

--- main.go
+++ main.go
@@ -9,6 +9,27 @@ func main()  {
 	fmt.Println("Hello Guys") 
 	fmt.Println(&text) // & check memory address
 
+	var listOfArrary = [4]string{"sdf", "sfs", "abc", "q"}
+	fmt.Println(listOfArrary)
+
+
+	var arrary[2]string
+
+
+	arrary[0] = text + "Welcome"
+	arrary[1] = text + " from earth"
+	fmt.Println(arrary)
+	fmt.Println(arrary[0])
+	fmt.Println(arrary[1])
+
+	var welcome_user []string
+	//above is arrary. let's explore about slice, slice availabile dynamic size, abstraction of arrary
+	welcome_user = append(welcome_user,   "nice to meet you")
+	fmt.Println(len(welcome_user)) 
+
+
+
+
 	var username string
 	fmt.Scan(&username)
 	fmt.Printf(username)
